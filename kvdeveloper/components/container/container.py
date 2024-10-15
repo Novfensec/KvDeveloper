76a7b5cc0ad1de7a3f8cd48ec3c6139c2ecc890a
kartavya <87070473+Novfensec@users.noreply.github.com>
2024-10-15 10:31:26 +0530

    Implmented Container Component

--- kvdeveloper/components/container/container.py
+++ kvdeveloper/components/container/container.py
@@ -7,23 +7,22 @@ class Container(MDBoxLayout, StateLayerBehavior):
     type = OptionProperty(
         "fluid",
         options=("fluid", "small", "medium", "large"),
-    )
+        )
+    """
+    The type of the conatiner.  
+    """
 
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.orientation = "vertical"
-        self.padding = [10, 5, 10, 5]
-        self.bind(type=self.adjust_size)
-        self.adaptive_height = True
-        self.spacing = "10dp"
+    def __init__(self, *args, **kwargs):
+        super(Container, self).__init__(*args, **kwargs)
+        self.bind(size=self.adjust_size)
 
     def adjust_size(self, *args) -> None:
         if self.parent:
             if self.type == "small":
-                self.padding = [self.width * 0.9, 5, self.width * 0.9, 5]
+                self.padding = [self.width * 0.08, 10, self.width * 0.08, 10]
             elif self.type == "medium":
-                self.padding = [self.width * 0.6, 5, self.width * 0.6, 5]
+                self.padding = [self.width * 0.04, 10, self.width * 0.04, 10]
             elif self.type == "large":
-                self.padding = [self.width * 0.3, 5, self.width * 0.3, 5]
+                self.padding = [self.width * 0.01, 10, self.width * 0.01, 10]
             elif self.type == "fluid":
                 pass
