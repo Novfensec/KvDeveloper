6983fef0172a335049d62037c313f31762b3864e
kartavya <87070473+Novfensec@users.noreply.github.com>
2024-10-21 17:47:56 +0530

    add settings1 layout

---
+++ kvdeveloper/layouts/Settings/1/extensions.py
@@ -0,0 +1,8 @@
+from kivymd.uix.relativelayout import MDRelativeLayout
+from kivy.properties import StringProperty, BooleanProperty
+
+class IconListItem(MDRelativeLayout):
+    text = StringProperty()
+    icon = StringProperty()
+    secondary_text = StringProperty()
+    ripple = BooleanProperty(True)
